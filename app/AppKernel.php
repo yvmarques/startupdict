6a506381e48d3409121ef3a6b92861ea2fa91dea
Yvan Marques <yvmarques@gmail.com>
2014-04-16 20:02:19 +0200

    remove buggy bundle for now

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Infuse\DictionaryBundle\InfuseDictionaryBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            // new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
